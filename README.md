1ce82675c268614e7d06cabd336fbc7e1068bcaf
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-05-31 21:09:20 +0800

    first

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # SwiftyOnAppear
 
-A description of this package.
+`SwiftyOnAppear` provides you several functions that can used in the SwiftUI. It provides the `UIKit` view life circle functions.
+
+## How to use
+
+```swift
+Text("Hello, world!")
+    .padding()
+    .viewDidLoad {
+        print("viewDidLoad")
+    }
+    .viewWillDisappear { animated in
+        print("viewWillDisappear", animated)
+    }
+    .viewDidDisappear { animated in
+        print("viewDidDisappear", animated)
+    }
+    .viewWillAppear { animated in
+        print("viewWillAppear", animated)
+    }
+    .viewDidAppear { animated in
+        print("viewDidAppear", animated)
+    }
+    .viewDidLayoutSubviews {
+        print("viewDidLayoutSubviews")
+    }
+    .viewWillLayoutSubviews {
+        print("viewWillLayoutSubviews")
+    }
+```
--- Sources/SwiftyOnAppear/SwiftyOnAppear.swift
+++ Sources/SwiftyOnAppear/SwiftyOnAppear.swift
@@ -1,6 +1,155 @@
-public struct SwiftyOnAppear {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+class SwiftyOnAppearViewController: UIViewController {
+    var delegate: SwiftyOnAppearViewDelegate? = nil
+
+    override func viewDidLoad() {
+        delegate?.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        delegate?.viewDidAppear(animated)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        delegate?.viewWillAppear(animated)
+    }
+
+    override func viewDidLayoutSubviews() {
+        delegate?.viewDidLayoutSubviews()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        delegate?.viewDidDisappear(animated)
+    }
+
+    override func viewWillLayoutSubviews() {
+        delegate?.viewWillLayoutSubviews()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        delegate?.viewWillDisappear(animated)
+    }
+}
+
+protocol SwiftyOnAppearViewDelegate {
+    func viewDidLoad()
+    func viewDidAppear(_ animated: Bool)
+    func viewWillAppear(_ animated: Bool)
+    func viewDidLayoutSubviews()
+    func viewDidDisappear(_ animated: Bool)
+    func viewWillLayoutSubviews()
+    func viewWillDisappear(_ animated: Bool)
+}
+
+struct SwiftyOnAppearView: UIViewControllerRepresentable {
+    let viewDidLoad: (() -> ())?
+    let viewDidAppear: ((_ animated: Bool) -> ())?
+    let viewWillAppear: ((_ animated: Bool) -> ())?
+    let viewDidLayoutSubviews: (() -> ())?
+    let viewDidDisappear: ((_ animated: Bool) -> ())?
+    let viewWillLayoutSubviews: (() -> ())?
+    let viewWillDisappear: ((_ animated: Bool) -> ())?
+
+    class Coordinator: NSObject, SwiftyOnAppearViewDelegate {
+        var parent: SwiftyOnAppearView
+
+        init(_ parent: SwiftyOnAppearView) {
+            self.parent = parent
+        }
+
+        func viewDidLoad() {
+            parent.viewDidLoad?()
+        }
+
+        func viewDidAppear(_ animated: Bool) {
+            parent.viewDidAppear?(animated)
+        }
+
+        func viewWillAppear(_ animated: Bool) {
+            parent.viewWillAppear?(animated)
+        }
+
+        func viewDidLayoutSubviews() {
+            parent.viewDidLayoutSubviews?()
+        }
+
+        func viewDidDisappear(_ animated: Bool) {
+            parent.viewDidDisappear?(animated)
+        }
+
+        func viewWillLayoutSubviews() {
+            parent.viewWillLayoutSubviews?()
+        }
+
+        func viewWillDisappear(_ animated: Bool) {
+            parent.viewWillDisappear?(animated)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> SwiftyOnAppearViewController {
+        let controller = SwiftyOnAppearViewController()
+        controller.delegate = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(_ nsViewController: SwiftyOnAppearViewController, context: Context) {
+
+    }
+}
+
+struct SwiftyModifier: ViewModifier {
+    let viewDidLoad: (() -> ())?
+    let viewDidAppear: ((_ animated: Bool) -> ())?
+    let viewWillAppear: ((_ animated: Bool) -> ())?
+    let viewDidLayoutSubviews: (() -> ())?
+    let viewDidDisappear: ((_ animated: Bool) -> ())?
+    let viewWillLayoutSubviews: (() -> ())?
+    let viewWillDisappear: ((_ animated: Bool) -> ())?
+
+    func body(content: Content) -> some View {
+        content
+            .background(SwiftyOnAppearView(viewDidLoad: viewDidLoad, viewDidAppear: viewDidAppear, viewWillAppear: viewWillAppear, viewDidLayoutSubviews: viewDidLayoutSubviews, viewDidDisappear: viewDidDisappear, viewWillLayoutSubviews: viewWillLayoutSubviews, viewWillDisappear: viewWillDisappear))
+    }
+}
+
+public extension View {
+    func viewDidLoad(_ perform: @escaping () -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: perform, viewDidAppear: nil, viewWillAppear: nil, viewDidLayoutSubviews: nil, viewDidDisappear: nil, viewWillLayoutSubviews: nil, viewWillDisappear: nil))
+    }
+
+    func viewDidAppear(_ perform: @escaping (_ animated: Bool) -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: perform, viewWillAppear: nil, viewDidLayoutSubviews: nil, viewDidDisappear: nil, viewWillLayoutSubviews: nil, viewWillDisappear: nil))
+    }
+
+    func viewWillAppear(_ perform: @escaping (_ animated: Bool) -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: nil, viewWillAppear: perform, viewDidLayoutSubviews: nil, viewDidDisappear: nil, viewWillLayoutSubviews: nil, viewWillDisappear: nil))
+    }
+
+    func viewDidLayoutSubviews(_ perform: @escaping () -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: nil, viewWillAppear: nil, viewDidLayoutSubviews: perform, viewDidDisappear: nil, viewWillLayoutSubviews: nil, viewWillDisappear: nil))
+    }
+
+    func viewDidDisappear(_ perform: @escaping (_ animated: Bool) -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: nil, viewWillAppear: nil, viewDidLayoutSubviews: nil, viewDidDisappear: perform, viewWillLayoutSubviews: nil, viewWillDisappear: nil))
+    }
+
+    func viewWillLayoutSubviews(_ perform: @escaping () -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: nil, viewWillAppear: nil, viewDidLayoutSubviews: nil, viewDidDisappear: nil, viewWillLayoutSubviews: perform, viewWillDisappear: nil))
+    }
+
+    func viewWillDisappear(_ perform: @escaping (_ animated: Bool) -> ()) -> some View {
+        self
+            .modifier(SwiftyModifier(viewDidLoad: nil, viewDidAppear: nil, viewWillAppear: nil, viewDidLayoutSubviews: nil, viewDidDisappear: nil, viewWillLayoutSubviews: nil, viewWillDisappear: perform))
     }
 }
